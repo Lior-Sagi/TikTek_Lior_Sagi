bdc597ae9585aad98493273b4ea72c5966855dd0
Ariel <arielvdd@gmail.com>
2025-02-09 11:10:02 +0200

    9.2.25 on Search.java working. continue onItemSelected

---
+++ app/src/main/java/com/example/tiktek_lior_sagi/adapter/BookSpinnerAdapter.java
@@ -0,0 +1,101 @@
+package com.example.tiktek_lior_sagi.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.tiktek_lior_sagi.R;
+import com.example.tiktek_lior_sagi.model.Book;
+import com.example.tiktek_lior_sagi.utils.ImageUtil;
+
+import java.util.List;
+
+/// Adapter for the Book spinner
+/// @see ArrayAdapter
+/// @see Book
+
+public class BookSpinnerAdapter extends ArrayAdapter<Book> {
+
+    /// inflater for the layout
+    /// @see LayoutInflater
+    private final LayoutInflater inflater;
+    /// resource for the layout
+    /// @see android.R.layout#simple_spinner_item
+    private final int resource;
+
+    /// constructor
+    /// @param context the context
+    /// @param resource the resource
+    /// @param objects the list of objects
+    public BookSpinnerAdapter(Context context, int resource, List<Book> objects) {
+        super(context, resource, objects);
+        this.inflater = LayoutInflater.from(context);
+        this.resource = resource;
+    }
+
+    /// get the view for the spinner
+    /// @param position the position of the item in the list
+    /// @param convertView the view to convert
+    /// @param parent the parent view group
+    /// @return the view for the spinner
+    /// @see View
+    /// @see ViewGroup
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(resource, parent, false);
+        }
+        Book Book = getItem(position);
+        if (Book == null) {
+            return convertView;
+        }
+
+        /// Set the text for the TextView
+        TextView textView = convertView.findViewById(android.R.id.text1);
+
+        textView.setText(Book.getBook());
+
+        return convertView;
+    }
+
+    /// get the view for the dropdown
+    /// @param position the position of the item in the list
+    /// @param convertView the view to convert
+    /// @param parent the parent view group
+    /// @return the view for the dropdown
+    /// @see View
+    /// @see ViewGroup
+    @Override
+    public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+        /// inflate the item_selected_Book layout
+        if (convertView == null) {
+            /// @see R.layout#item_selected_Book
+            convertView = inflater.inflate(R.layout.itemselectedbook, parent, false);
+        }
+        /// get the Book at the position
+        Book Book = getItem(position);
+
+        /// return the view if the Book is null
+        if (Book == null) return convertView;
+
+        /// Set the text for the TextView
+     //   TextView textView = convertView.findViewById(R.id.Book_name_text_view);
+      //  textView.setText(Book.getName());
+
+        /// Set the image for the ImageView
+      //  ImageView imageView = convertView.findViewById(R.id.Book_image_view);
+        /// convert the image from base64 to bitmap
+        /// @see ImageUtil#convertFrom64base(String)
+    //    imageView.setImageBitmap(ImageUtil.convertFrom64base(Book.getImageBase64()));
+
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/tiktek_lior_sagi/screens/Search.java
+++ app/src/main/java/com/example/tiktek_lior_sagi/screens/Search.java
@@ -2,8 +2,10 @@ package com.example.tiktek_lior_sagi.screens;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -16,20 +18,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tiktek_lior_sagi.R;
+import com.example.tiktek_lior_sagi.adapter.BookSpinnerAdapter;
 import com.example.tiktek_lior_sagi.model.Book;
 import com.example.tiktek_lior_sagi.services.DatabaseService;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-public class Search extends AppCompatActivity implements View.OnClickListener {
-    Spinner spSubject;
-    Spinner spBook;
-    Spinner spPage;
-    Spinner spQuestion;
-    List<Book> bookList=new ArrayList<>();
-    String[] books={getBooks(bookList)};
+public class Search extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+    Spinner spSubject, spBook, spPage, spQuestion;
+
+    String subject;
+
+
+    //List<Book> bookList=new ArrayList<>();
+    DatabaseService.DatabaseCallback<List<Book>> bookList;
     Button btnSearch;
+
+
+    /// tag for logging
+    private static final String TAG = "SearchBook";
+
+
+    private BookSpinnerAdapter bookSpinnerAdapter;
+    //  private BooksAdapter booksAdapter;
+    private List<Book> selectedBooks;
+
+    List<Book> allBooks;
+    private DatabaseService databaseService;
+    private ArrayList<Book> serchSubject;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,16 +59,60 @@ public class Search extends AppCompatActivity implements View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        spSubject=findViewById(R.id.spSubject);
-        spBook=findViewById(R.id.spBook);
-        spPage=findViewById(R.id.spPage);
-        spQuestion=findViewById(R.id.spQuestion);
-        btnSearch=findViewById(R.id.btnSearch);
+        initViews();
+        /// get the instance of the database service
+        databaseService = DatabaseService.getInstance();
+        bookSpinnerAdapter = new BookSpinnerAdapter(Search.this, android.R.layout.simple_spinner_item, allBooks);
+        //   bookSpinner.setAdapter(bookSpinnerAdapter);
+        /// get all the books from the database
+        databaseService.getBooks(new DatabaseService.DatabaseCallback<List<Book>>() {
+            @Override
+            public void onCompleted(List<Book> object) {
+                Log.d(TAG, "onCompleted: " + object);
+                allBooks.clear();
+                allBooks.addAll(object);
+                /// notify the adapter that the data has changed
+                /// this specifies that the data has changed
+                /// and the adapter should update the view
+                /// @see BookSpinnerAdapter#notifyDataSetChanged()
+                // bookSpinnerAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onFailed(Exception e) {
+                Log.e(TAG, "onFailed: ", e);
+            }
+        });
+    }
+    private void initViews() {
+
+        spSubject = findViewById(R.id.spSubject);
+        spSubject.setOnItemSelectedListener(this);
+        spBook = findViewById(R.id.spBook);
+        spPage = findViewById(R.id.spPage);
+        spQuestion = findViewById(R.id.spQuestion);
+        btnSearch = findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(this);
     }
-
     @Override
     public void onClick(View v) {
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        subject = (String) parent.getItemAtPosition(position);
+
+
+        //   eSearch1 = etSearch1.getText().toString() + "";
+        for (int i = 0; i < allBooks.size(); i++) {
+            Book current1 = allBooks.get(i);
+            if (current1.getSubject().equals(subject)) {
+                selectedBooks.add(current1);
+            }
+        }
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
